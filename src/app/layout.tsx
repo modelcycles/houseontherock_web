aa7f91362368da3c920166598e96d24403669176
modelcycles <whdydcjf0561@gmail.com>
2025-04-29 23:17:57 +0900

    Fix: Minor build failing issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
-import { HouseHeader } from "./_components/houseHeader";
+import { HouseHeader } from "./_components/HouseHeader";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
