24ef69c4cc6151fbcfcfc6c84d3e7abb6a2d0b58
modelcycles <whdydcjf0561@gmail.com>
2025-05-09 08:02:58 +0900

    Feat: Text update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 "use client";
 import { HouseBody } from "./_components/HouseBody";
+import "./css/page.css";
 
 export default function Home() {
   return (
     <HouseBody>
-      <div>사진책으로 감동을 전해드리는 출판사 하우스온더락입니다.</div>
+      <div className="main-text-korean" style={{ fontSize: 70 }}>
+        <p style={{ marginTop: "20%", marginBottom: 50 }}>사진책으로</p>
+        <p style={{ marginBottom: 50 }}>감동을 전달하는 출판사</p>
+        <p style={{ marginBottom: 50 }}>하우스온더락입니다.</p>
+      </div>
     </HouseBody>
   );
 }
