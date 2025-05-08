88d6b796afb678e80616a0f5709df908c0e9d052
modelcycles <whdydcjf0561@gmail.com>
2025-05-09 06:35:22 +0900

    Feat: Footer icon

--- src/app/_components/HouseFooter.tsx
+++ src/app/_components/HouseFooter.tsx
@@ -1,10 +1,29 @@
+import { BiLogoGmail } from "react-icons/bi";
+import { FaInstagram } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+
 export function HouseFooter() {
   return (
     // #dfd3c3
     <>
-      <div style={{ height: "50px", background: "white", color: "black" }}>
-        <div>Instagram : yongcheol_c</div>
-        <div>e-mail : houseontherock2025@gmail.com</div>
+      <div
+        style={{
+          height: "100px",
+          background: "#dfd3c3",
+          color: "black",
+          justifyItems: "center",
+          justifyContent: "center",
+          display: "flex",
+        }}
+      >
+        <div className="menu-button">
+          <FaInstagram size={24} />
+        </div>
+        <div className="menu-button">
+          <BiLogoGmail size={24} />
+        </div>
+        {/* <div>Instagram : yongcheol_c</div> */}
+        {/* <div>e-mail : houseontherock2025@gmail.com</div> */}
       </div>
     </>
   );
