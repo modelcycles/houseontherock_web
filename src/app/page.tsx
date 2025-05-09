ebd25b40f14f5014ff8b8eaf1b829253c7734032
modelcycles <whdydcjf0561@gmail.com>
2025-05-09 19:39:39 +0900

    Fix: Landing page margin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <HouseBody>
       <div className="main-text-korean" style={{ fontSize: 70 }}>
-        <p style={{ marginTop: "20%", marginBottom: 50 }}>사진책으로</p>
+        <p style={{ marginTop: "50px", marginBottom: 50 }}>사진책으로</p>
         <p style={{ marginBottom: 50 }}>감동을 전달하는 출판사</p>
         <p style={{ marginBottom: 50 }}>하우스온더락입니다.</p>
       </div>
