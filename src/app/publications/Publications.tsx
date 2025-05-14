efa54a2874f53e56b16ff4977c89b761c575b410
modelcycles <whdydcjf0561@gmail.com>
2025-05-14 17:38:12 +0900

    Feat: Publication page Device view

---
+++ src/app/publications/Publications.tsx
@@ -0,0 +1,169 @@
+import Link from "next/link";
+import { HouseBody } from "../_components/HouseBody";
+import { useMedia } from "../_components/MediaQueryContext";
+import "../css/page.css";
+import { useEffect, useState } from "react";
+
+export function PublishedBooks() {
+  const [isClient, setIsClient] = useState(false);
+  useEffect(() => {
+    setIsClient(true);
+  });
+
+  const { isDesktop, isMobile, isTablet } = useMedia();
+
+  if (!isClient) return null;
+
+  return (
+    <>
+      {isDesktop && <DesktopPublishedBooks />}
+      {isTablet && <TabletPublishedBooks />}
+      {isMobile && <MobilePublishedBooks />}
+    </>
+  );
+}
+
+function DesktopPublishedBooks() {
+  return (
+    <HouseBody align="left">
+      <div
+        className="main-text-english"
+        style={{
+          width: "100%",
+          fontSize: 70,
+          textAlign: "left",
+          // background: "#f5ede4",
+          display: "block",
+        }}
+      >
+        <p
+          style={{
+            marginBottom: "75px",
+            width: "100%",
+            borderBottom: "1px solid black",
+          }}
+        >
+          Publications
+        </p>
+        <div style={{ width: "100%", justifyItems: "left" }}>
+          <ProductCard
+            imageSrc="/book_images/two_of_a_kind/thumbnail/two_of_a_kind_cover.jpg"
+            title={"Two Of A Kind | by Yongcheol Cho"}
+            size={"400px"}
+          ></ProductCard>
+        </div>
+      </div>
+    </HouseBody>
+  );
+}
+
+function TabletPublishedBooks() {
+  return (
+    <HouseBody align="center">
+      <div
+        className="main-text-english"
+        style={{
+          width: "100%",
+          fontSize: 70,
+          textAlign: "left",
+          // background: "#f5ede4",
+          display: "block",
+        }}
+      >
+        <p
+          style={{
+            marginBottom: "75px",
+            width: "100%",
+            borderBottom: "1px solid black",
+          }}
+        >
+          Publications
+        </p>
+        <div style={{ width: "100%", justifyItems: "center" }}>
+          <ProductCard
+            imageSrc="/book_images/two_of_a_kind/thumbnail/two_of_a_kind_cover.jpg"
+            title={"Two Of A Kind | by Yongcheol Cho"}
+            size={"400px"}
+          ></ProductCard>
+        </div>
+      </div>
+    </HouseBody>
+  );
+}
+
+function MobilePublishedBooks() {
+  return (
+    <HouseBody align="center">
+      <div
+        className="main-text-english"
+        style={{
+          width: "100%",
+          fontSize: 70,
+          textAlign: "left",
+          // background: "#f5ede4",
+          display: "block",
+        }}
+      >
+        <p
+          style={{
+            marginBottom: "75px",
+            width: "100%",
+            borderBottom: "1px solid black",
+          }}
+        >
+          Publications
+        </p>
+        <div style={{ width: "100%", justifyItems: "center" }}>
+          <ProductCard
+            imageSrc="/book_images/two_of_a_kind/thumbnail/two_of_a_kind_cover.jpg"
+            title={"Two Of A Kind | by Yongcheol Cho"}
+            size={"55%"}
+          ></ProductCard>
+        </div>
+      </div>
+    </HouseBody>
+  );
+}
+
+function ProductCard({
+  imageSrc,
+  title,
+  size,
+}: {
+  imageSrc: string;
+  title: string;
+  size?: string;
+}) {
+  return (
+    <div
+      style={{
+        width: size ? size : "400px",
+        textAlign: "left",
+        // padding: "25px",
+        // border: "1px solid black",
+      }}
+    >
+      <Link href="/publications/twoofakind">
+        <div
+          style={{
+            width: "100%",
+            paddingTop: "100%",
+            backgroundImage: `url(${imageSrc})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            borderRadius: "4px",
+          }}
+        ></div>
+        <p
+          style={{
+            marginTop: "10px",
+            fontSize: "15px",
+            fontWeight: "normal",
+          }}
+        >
+          {title}
+        </p>
+      </Link>
+    </div>
+  );
+}
--- src/app/publications/page.tsx
+++ src/app/publications/page.tsx
@@ -1,72 +1,11 @@
-import Link from "next/link";
-import { HouseBody } from "../_components/HouseBody";
-import "../css/page.css";
+"use client";
 import React from "react";
+import { PublishedBooks } from "./Publications";
 
-export default function PublishedBooks() {
+export default function BodyWrapPublishedBooks() {
   return (
-    <HouseBody align="center">
-      <div
-        className="main-text-english"
-        style={{
-          width: "100%",
-          fontSize: 70,
-          textAlign: "left",
-          // background: "#f5ede4",
-          display: "block",
-        }}
-      >
-        <p
-          style={{
-            marginBottom: "75px",
-            width: "100%",
-            borderBottom: "1px solid black",
-          }}
-        >
-          Publications
-        </p>
-        <div style={{ width: "100%", justifyItems: "center" }}>
-          <ProductCard
-            imageSrc="/book_images/two_of_a_kind/thumbnail/two_of_a_kind_cover.jpg"
-            title={"Two Of A Kind | by Yongcheol Cho"}
-          ></ProductCard>
-        </div>
-      </div>
-    </HouseBody>
-  );
-}
-
-function ProductCard({ imageSrc, title }: { imageSrc: string; title: string }) {
-  return (
-    <div
-      style={{
-        width: "400px",
-        textAlign: "left",
-        // padding: "25px",
-        // border: "1px solid black",
-      }}
-    >
-      <Link href="/publications/twoofakind">
-        <div
-          style={{
-            width: "100%",
-            paddingTop: "100%",
-            backgroundImage: `url(${imageSrc})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            borderRadius: "4px",
-          }}
-        ></div>
-        <p
-          style={{
-            marginTop: "10px",
-            fontSize: "15px",
-            fontWeight: "normal",
-          }}
-        >
-          {title}
-        </p>
-      </Link>
-    </div>
+    <>
+      <PublishedBooks />
+    </>
   );
 }
